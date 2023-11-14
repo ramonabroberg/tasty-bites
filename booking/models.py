d544b27f4105b8ff62be7544060da88030b6cc93
ramonabroberg <>
2023-11-14 17:07:37 +0000

    Add booking class in models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    number_of_people = models.IntegerField()
+    date = models.DateTimeField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField()
+    message = models.CharField(max_length=250)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.date} ({self.number_of_people} people)'
+
+    class Meta:
+        ordering = ['date']
