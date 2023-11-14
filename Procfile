148109d45d2a42cd2bd78a5bfbcfc648270ad302
ramonabroberg <>
2023-11-14 11:31:06 +0000

    deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tasty_bites.wsgi
--- tasty_bites/settings.py
+++ tasty_bites/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ramonabroberg-tasty-bite-exib8bm4yx.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['tasty-bites-rb-427304cf20b7.herokuapp.com/',
+                 '8000-ramonabroberg-tasty-bite-exib8bm4yx.us2.codeanyapp.com']
 
 
 # Application definition
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'tasty_bites.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,6 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
