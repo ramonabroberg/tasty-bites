473c266d852dc5dedd2c6bf67cd6ccdd47dc2d4a
ramonabroberg <>
2023-11-20 11:44:35 +0000

    Move menu to index.html and add allauth

--- tasty_bites/settings.py
+++ tasty_bites/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['tasty-bites-rb-427304cf20b7.herokuapp.com', '8000-ramonabroberg-tasty-bite-exib8bm4yx.us2.codeanyapp.com']
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Application definition
 
@@ -42,9 +43,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary',
     'booking',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'tasty_bites.urls'
