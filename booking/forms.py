4968ec91a7a7160770f31cfb98aca6405b1df6f7
ramonabroberg <>
2023-11-20 23:36:41 +0000

    Update account pages and css

--- booking/forms.py
+++ booking/forms.py
@@ -6,3 +6,6 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['number_of_people', 'date', 'first_name', 'last_name', 'phone', 'email', 'message']
+        widgets = {
+            'date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
