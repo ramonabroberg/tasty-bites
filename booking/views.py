85647e788ce5a83d1e627b6150e0f5a0e36d8ffe
ramonabroberg <>
2023-11-20 12:37:44 +0000

    Add go_to_booking_page view

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from .models import Booking
 
+
+@login_required
+def go_to_booking_page(request):
+    return redirect('booking_form')
--- tasty_bites/urls.py
+++ tasty_bites/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from booking.views import go_to_booking_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('accounts/', include('allauth.urls')),
+    path('go_to_booking_page/', go_to_booking_page, name='go_to_booking_page'),
 ]
